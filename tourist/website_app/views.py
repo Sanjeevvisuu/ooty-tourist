81bf789eff869d2828961366b239c08a383fb82c
sanjeev <sanjeevvisuu@gmail.com>
2024-01-15 09:01:59 +0530

    changes

--- tourist/website_app/views.py
+++ tourist/website_app/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+def about_us(request):
+    return render(request,"about_us.html")
+def package(request):
+    return render(request,"package.html")
