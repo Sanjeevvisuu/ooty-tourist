11b0c7f8485b1db1e70008dd36ceaba44319e3bb
techie hkr <codeer0@gmail.com>
2024-01-16 05:23:37 +0000

    Update file urls.py

--- tourist/tourist/urls.py
+++ tourist/tourist/urls.py
@@ -15,16 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from website_app.urls import *
-from product.urls import *
+from django.urls import path, include
+from website_app.urls import urlpatterns as website_app_urls
+from product.urls import urlpatterns as product_urls
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("website_app.urls")),
-    path("product",include("product.urls")),
-    path("booking",include("booking.urls"))
+    path("", include(website_app_urls, namespace='website')),
+    path("product", include(product_urls, namespace='product')),
+    path("booking", include("booking.urls", namespace='booking')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
