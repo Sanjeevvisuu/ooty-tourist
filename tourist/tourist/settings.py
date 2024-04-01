c993ecf03188695bac68927b1cc584be96a83641
Sanjeevvisuu <139469409+Sanjeevvisuu@users.noreply.github.com>
2024-04-01 20:39:15 +0530

    Update settings.py

--- tourist/tourist/settings.py
+++ tourist/tourist/settings.py
@@ -19,7 +19,7 @@ import os
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oq64j)^%8f-@&8@of@by1w_s$89yl(8p4g9sq!-(976b-*3=hh'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
